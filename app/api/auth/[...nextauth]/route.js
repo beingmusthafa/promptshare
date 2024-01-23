06570b671f6368500254d70b338e251a98700bcd
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-01-23 19:00:54 +0530

    minor fix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -25,7 +25,7 @@ const handler = NextAuth({
             email: profile.email,
             username: profile.name.replace(" ", "").toLowerCase(),
             image: profile.image,
-          });
+          }).save();
         }
         return true;
       } catch (error) {
