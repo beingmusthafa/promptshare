0c167aa46c9d4723844880bb7101690a42dc0ff1
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-01-24 08:40:06 +0530

    placeholder image

--- models/user.js
+++ models/user.js
@@ -13,6 +13,8 @@ const userSchema = new Schema({
   },
   image: {
     type: String,
+    default:
+      "https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg?size=338&ext=jpg&ga=GA1.1.632798143.1705881600&semt=ais",
   },
 });
 
